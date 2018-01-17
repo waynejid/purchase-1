36d41e8c919d8cc96a0987aed3390efb251d018a
Valentin Thirion <vt@abakusitsolutions.eu>
2018-01-17 16:28:59 +0100

    [ADD] First module

---
+++ product_price_restricted/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Price Restricted",
+
+    'summary': """
+    """,
+
+    'description': """
+        Product Price Restricted to Sales Manager
+        
+        Sets the view of the cost price only visible by Sales Manager
+        
+        This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
+    """,
+
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+
+    'category': 'Sale',
+    'version': '10.0.1.0',
+
+    'depends': [
+        'sale'
+    ],
+
+    'data': [
+        'views/product_template.xml',
+    ],
+}
