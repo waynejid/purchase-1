09c5473dc0e805ff514365f6b20c851fe82ed2f2
Valentin Thirion <vt@abakusitsolutions.eu>
2018-01-17 16:01:06 +0100

    [ADD] Added 'last update view'

--- product_price_history/models/product_template.py
+++ product_price_history/models/product_template.py
@@ -2,15 +2,25 @@
 
 from odoo import models, fields, api, exceptions, _
 
-# import logging
-# _logger = logging.getLogger(__name__)
+import logging
+_logger = logging.getLogger(__name__)
     
     
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
     price_history_count = fields.Integer(string="Price History Count", compute='_compute_price_history_count')
+    cost_price_last_update_text = fields.Char(string="Last Cost Price Update", compute='_compute_cost_price_last_update')
     
     @api.one
     def _compute_price_history_count(self):
         self.price_history_count = len(self.env['product.price.history'].search([('product_id.product_tmpl_id.id', '=', self.id)]))
+
+    @api.one
+    def _compute_cost_price_last_update(self):
+        history_ids = self.env['product.price.history'].search([('product_id.product_tmpl_id.id', '=', self.id)], order='datetime DESC', limit=1)
+        if len(history_ids) == 0:
+            self.cost_price_last_update_text = str("Never")
+        else:
+            self.cost_price_last_update_text = str(history_ids.datetime)
+        
