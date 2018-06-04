d53b94c8201282150ac11f865396e420de29a240
Valentin Thirion <vt@abakusitsolutions.eu>
2018-06-04 17:33:43 +0200

    [ADD] First version

---
+++ purchase_order_report_abakus/__openerp__.py
@@ -0,0 +1,21 @@
+﻿{
+    'name': "Purchase Order Report for AbAKUS",
+    'version': '9.0.1.0',
+    'depends': [
+        'purchase',
+    ],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Purchase',
+    'description': 
+    """
+Purchase Order Report for AbaKUS.
+    
+    - This module changes the report from PO to a design for AbAKUS
+
+This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
+    """,
+    'data': [
+        'reports/purchase_order_report.xml',
+    ],
+}
