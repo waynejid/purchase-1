345dd421c75ebfd15ad50b792f8d68a6e7af53ba
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-08-12 12:17:50 +0200

    Migration to 10.0: product_price_restricted (conforming)

---
+++ product_price_restricted/README.rst
@@ -0,0 +1,28 @@
+=====================================
+   Product Price Restricted to Sales Manager
+=====================================
+
+This package is the an add on to ``purchase``.
+
+Sets the view of the cost price only visible by Sales Manager
+
+Installation notes
+==================
+
+Credits
+=======
+
+Contributors
+------------
+
+* Valentin Thirion <valentin.thirion@abakusitsolutions.eu>
+* Paul Ntabuye Butera <paul.n.butera@abakusitsolutions.eu>
+
+Maintainer
+-----------
+
+.. image:: http://www.abakusitsolutions.eu/wp-content/themes/abakus/images/logo.gif
+   :alt: AbAKUS IT SOLUTIONS
+   :target: http://www.abakusitsolutions.eu
+
+This module is maintained by AbAKUS IT SOLUTIONS
--- product_price_restricted/__manifest__.py
+++ product_price_restricted/__manifest__.py
@@ -1,28 +1,15 @@
 # -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
 {
     'name': "Product Price Restricted",
-
-    'summary': """
-    """,
-
-    'description': """
-        Product Price Restricted to Sales Manager
-        
-        Sets the view of the cost price only visible by Sales Manager
-        
-        This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
-    """,
-
-    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'version': '10.0.1.0.0',
+    'author': "AbAKUS it-solutions SARL",
+    'license': 'AGPL-3',
     'website': "http://www.abakusitsolutions.eu",
-
-    'category': 'Sale',
-    'version': '10.0.1.0',
-
     'depends': [
         'sale'
     ],
-
+    'category': 'Sale',
     'data': [
         'views/product_template.xml',
     ],
