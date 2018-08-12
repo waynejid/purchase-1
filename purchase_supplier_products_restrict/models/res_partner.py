55f08815afabf251a8e448b021adbdd4d509497a
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-08-12 13:06:37 +0200

    Migration 10.0: purchase_supplier_products_restrict (conforming)

--- purchase_supplier_products_restrict/models/res_partner.py
+++ purchase_supplier_products_restrict/models/res_partner.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-
-from openerp import models, fields, api, exceptions, _
-
+# (c) AbAKUS IT Solutions
 import logging
+from odoo import models, fields, api, exceptions, _
+
 _logger = logging.getLogger(__name__)
-    
-    
+
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    
-    vendor_products_count = fields.Integer(string="Vendor Products Count", compute='_compute_vendor_products_count')
-    
+
+    vendor_products_count = fields.Integer(compute='_compute_vendor_products_count')
+
     @api.one
     def _compute_vendor_products_count(self):
-        self.vendor_products_count = len(self.env['product.template'].search([('seller_ids.name', '=', self.id)]))
+        self.vendor_products_count = self.env['product.template'].search_count([('seller_ids.name', '=', self.id)])
