ddb30b6c694e233cacd171c241ae37c5ae2d54a6
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-08-12 12:10:26 +0200

    Migration to 10.0: product_price_history (conforming)

--- product_price_history/__manifest__.py
+++ product_price_history/__manifest__.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
 {
     'name': "Product Price History",
-
-    'summary': """
-    """,
-
-    'description': """
-        Product Price History
-        
-        Adds a view to display the price history of a product
-        
-        This module has been developed by Jason PINDAT, intern @ AbAKUS it-solutions.
-    """,
-
-    'author': "Jason PINDAT, AbAKUS it-solutions SARL",
+    'version': '10.0.1.0.0',
+    'author': "AbAKUS it-solutions SARL",
+    'license': 'AGPL-3',
     'website': "http://www.abakusitsolutions.eu",
-
-    'category': 'Sale',
-    'version': '10.0.1.0',
-
     'depends': [
         'base',
         'sale'
     ],
-
+    'category': 'Sale',
     'data': [
         'views/product_price_history.xml',
         'views/product_template.xml'
--- product_price_history/models/product_template.py
+++ product_price_history/models/product_template.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
-
+# (c) AbAKUS IT Solutions
+import logging
 from odoo import models, fields, api, exceptions, _
 
-import logging
 _logger = logging.getLogger(__name__)
-    
-    
+
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    
-    price_history_count = fields.Integer(string="Price History Count", compute='_compute_price_history_count')
-    cost_price_last_update_text = fields.Char(string="Last Cost Price Update", compute='_compute_cost_price_last_update')
-    
+
+    price_history_count = fields.Integer(compute='_compute_price_history_count')
+    cost_price_last_update_text = fields.Char(string="Last Cost Price Update",
+                                              compute='_compute_cost_price_last_update')
+
     @api.one
     def _compute_price_history_count(self):
-        self.price_history_count = len(self.env['product.price.history'].search([('product_id.product_tmpl_id.id', '=', self.id)]))
+        self.price_history_count = self.env['product.price.history'].search_count([
+            ('product_id.product_tmpl_id.id', '=', self.id)
+        ])
 
     @api.one
     def _compute_cost_price_last_update(self):
-        history_ids = self.env['product.price.history'].search([('product_id.product_tmpl_id.id', '=', self.id)], order='datetime DESC', limit=1)
+        history_ids = self.env['product.price.history'].search([
+            ('product_id.product_tmpl_id.id', '=', self.id)], order='datetime DESC', limit=1)
         if len(history_ids) == 0:
             self.cost_price_last_update_text = str("Never")
         else:
             self.cost_price_last_update_text = str(history_ids.datetime)
-        
