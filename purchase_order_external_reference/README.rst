f26532f118d5a9673faae511fe47acb67cf58d88
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-08-12 12:52:30 +0200

    Migration 10.0: purchase_order_external_reference (conforming)

---
+++ purchase_order_external_reference/README.rst
@@ -0,0 +1,28 @@
+=====================================
+   Purchase Order External Reference
+=====================================
+
+This package is the an add on to ``purchase``.
+
+This module adds a the partner external reference in the PO and in the report associated.
+
+Installation notes
+==================
+
+Credits
+=======
+
+Contributors
+------------
+
+* Valentin Thirion <valentin.thirion@abakusitsolutions.eu>
+* Paul Ntabuye Butera <paul.n.butera@abakusitsolutions.eu>
+
+Maintainer
+-----------
+
+.. image:: http://www.abakusitsolutions.eu/wp-content/themes/abakus/images/logo.gif
+   :alt: AbAKUS IT SOLUTIONS
+   :target: http://www.abakusitsolutions.eu
+
+This module is maintained by AbAKUS IT SOLUTIONS
--- purchase_order_external_reference/__manifest__.py
+++ purchase_order_external_reference/__manifest__.py
@@ -1,30 +1,17 @@
 # -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
 {
     'name': "Purchase Order External Reference",
-
-    'summary': """
-    """,
-
-    'description': """
-        Purchase Order External Reference
-
-        This module adds a the partner external reference in the PO and in the report associated.
-        
-        This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
-    """,
-
-    'author': "Valent THIRION, AbAKUS it-solutions SARL",
+    'version': '10.0.1.0.0',
+    'author': "AbAKUS it-solutions SARL",
+    'license': 'AGPL-3',
     'website': "http://www.abakusitsolutions.eu",
-
     'category': 'Product',
-    'version': '10.0.1.0',
-
     'depends': [
         'base',
         'purchase',
         'partner_reference_external',
     ],
-
     'data': [
         'views/purchase_order.xml',
         'reports/purchase_order_reports.xml',
