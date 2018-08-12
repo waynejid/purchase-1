ddb0652bd6b9eb5513d73db0047021f602bb9220
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-08-12 12:59:31 +0200

    Migration 10.0: purchase_supplier_certified (conforming)

--- purchase_supplier_certified/__manifest__.py
+++ purchase_supplier_certified/__manifest__.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
+# (c) AbAKUS IT Solutions
 {
     'name': "Certified Supplier",
-
-    'summary': """
-    """,
-
-    'description': """
-        Certified Supplier
-
-        Adds a boolean 'certified' on the supplier form and allows to search on them.
-
-        This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
-    """,
-
-    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'version': '10.0.1.0.0',
+    'author': "AbAKUS it-solutions SARL",
+    'license': 'AGPL-3',
     'website': "http://www.abakusitsolutions.eu",
-
     'category': 'Purchase',
-    'version': '10.0.1.0',
-
     'depends': [
         'base',
         'purchase'
     ],
-
     'data': [
         'views/res_partner_view.xml'
     ],
