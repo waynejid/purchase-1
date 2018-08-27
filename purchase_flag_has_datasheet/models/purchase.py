5159ad40025e0fa5652ee31fcc9f4a878bc4d306
Valentin Thirion <vt@abakusitsolutions.eu>
2018-08-27 10:41:42 +0200

    [FIX] Set default = False

--- purchase_flag_has_datasheet/models/purchase.py
+++ purchase_flag_has_datasheet/models/purchase.py
@@ -8,4 +8,4 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    has_datasheet = fields.Boolean(default=True)
+    has_datasheet = fields.Boolean(default=False)
