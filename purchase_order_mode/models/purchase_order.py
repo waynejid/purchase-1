530bb644fefa0401a43c8805f75346e1cb504284
Valentin Thirion <valentinthirion@gmail.com>
2018-10-25 10:22:57 +0200

    [FIX] Set required on the view and not the model

--- purchase_order_mode/models/purchase_order.py
+++ purchase_order_mode/models/purchase_order.py
@@ -8,4 +8,4 @@ _logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    purchase_mode = fields.Selection([('email', 'Email'), ('fax', 'Fax'), ('paper', 'Paper'), ('phone', 'Phone'), ('webstore', 'Webstore')], required=True, string="Purchase Mode")
+    purchase_mode = fields.Selection([('email', 'Email'), ('fax', 'Fax'), ('paper', 'Paper'), ('phone', 'Phone'), ('webstore', 'Webstore')], string="Purchase Mode")
